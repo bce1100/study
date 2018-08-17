52370b041b001088f9891d386ac3af246f1d133c
bce1100 <bce1100@126.com>
2018-08-17 17:25:11 +0800

    mybatis和spring整合

--- mybatisdemo/src/main/java/com/ljj/dao/UserMapper.java
+++ mybatisdemo/src/main/java/com/ljj/dao/UserMapper.java
@@ -2,10 +2,13 @@ package com.ljj.dao;
 
 import com.ljj.entity.User;
 
+import java.util.List;
+
 /**
  * @author bce1100
  * @date 2018/8/16.
  */
 public interface UserMapper {
     public User findUserById(int id) throws Exception;
+    public List<User> findAll();
 }
--- mybatisdemo/src/main/java/com/ljj/test/Test.java
+++ mybatisdemo/src/main/java/com/ljj/test/Test.java
@@ -4,6 +4,10 @@ import com.ljj.dao.UserMapper;
 import com.ljj.db.SessionFactoryUtil;
 import com.ljj.entity.User;
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 /**
  * @author bce1100
@@ -11,9 +15,14 @@ import org.apache.ibatis.session.SqlSession;
  */
 public class Test {
     public static void main(String[] args) throws Exception {
-        SqlSession session = SessionFactoryUtil.getSession();
+/*        SqlSession session = SessionFactoryUtil.getSession();
         UserMapper mapper = session.getMapper(UserMapper.class);
-        User user = mapper.findUserById(1);
-        System.out.println(user.getUserName()+":"+user.getPassword());
+        User user = mapper.findUserById(1);*/
+        ApplicationContext applicationContext =new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserMapper userMapper = applicationContext.getBean(UserMapper.class);
+        List<User> uList = userMapper.findAll();
+        for(User u:uList){
+            System.out.println(u.getId()+"->"+u.getUserName()+":"+u.getPassword());
+        }
     }
 }
