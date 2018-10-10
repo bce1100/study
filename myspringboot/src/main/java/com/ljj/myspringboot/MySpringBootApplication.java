a9f1a05d30b741c0c98a800a6883fac7b5541d40
bce1100 <bce1100@126.com>
2018-10-10 10:14:12 +0800

    10-10 尝试SpringBoot

---
+++ myspringboot/src/main/java/com/ljj/myspringboot/MySpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.ljj.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bce1100
+ * @date 2018/10/10.
+ */
+//说明这是一个springboot应用的入口类
+@SpringBootApplication
+public class MySpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class);
+    }
+}
---
+++ myspringboot/src/main/java/com/ljj/myspringboot/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.ljj.myspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author bce1100
+ * @date 2018/10/10.
+ */
+@Controller
+public class MyController {
+    @RequestMapping("/out")
+    @ResponseBody
+    public String out(){
+        return "success";
+    }
+}
